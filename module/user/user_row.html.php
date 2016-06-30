93c91dcb62baf504048c753ba62d0de4290adda7
Houlaymatou <houlay.bah@gmail.com>
2016-06-30 11:31:23 +0200

    suprression

--- module/user/user_row.html.php
+++
@@ -1,5 +0,0 @@
-<th scope="row"><?php print $user['uid']; ?></th>
-<td><?php print $user['user_name']; ?></td>
-<td><?php print $user['user_mail']; ?></td>
-<td><?php print $user['user_roles']; ?></td>
-       
